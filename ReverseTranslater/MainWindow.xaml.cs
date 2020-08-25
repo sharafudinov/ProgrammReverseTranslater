a7f17e2aabf47c1d7b978693fc88768c23222163
isharafudinov@inbox.ru <isharafudinov@inbox.ru>
2020-08-25 16:30:35 +0700

    Добавьте файлы проекта.

---
+++ ReverseTranslater/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReverseTranslater
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WorkClassWithWord WCWW = new WorkClassWithWord();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] TempBuffer = WCWW.InsertStringMass(TxtBox1_insert.Text);
+            if (WCWW.TestStringCorrectValue(TempBuffer) == true)
+            {
+                string[] ResultedArray = WCWW.Decoder(TempBuffer);
+                for (int i = 0; i < ResultedArray.Length; i++)
+                {
+                    TxtBox1_Result.Text += ResultedArray[i] + " ";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введены некоректные данные для перевода");
+            }
+        }
+
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            TxtBox1_Result.Text = "";
+            TxtBox1_insert.Text = "";
+        }
+    }
+}
---
+++ ReverseTranslater/WorkClassWithWord.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseTranslater
+{
+    class WorkClassWithWord
+    {
+        public string[] InsertStringMass(string Insert)
+        {
+            return Insert.Split(new[] { ' ', ']', '[',  '(', ')', '&' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        char buffer;
+        public string[] Decoder(string[] Inserted)
+        {
+                Char[] StringForDecoder = new Char[] { 'у', 'е', 'ы', 'а', 'о', 'э', 'я', 'и', 'ю', 'ё', 'У', 'Е', 'Ы', 'А', 'О', 'Э', 'Я', 'И', 'Ю', 'Ё' };
+                for (int i = 0; i < Inserted.Length; i++)
+                {
+                    string temp = Inserted[i];
+                    string temp2 = string.Empty;
+                    for (int j = 0; j < temp.Length; j++)
+                    {
+                        for (int x = 0; x < StringForDecoder.Length; x++)
+                        {
+                            if (temp[j] == StringForDecoder[x])
+                            {
+                                buffer = temp[j];
+                                j += 2;
+                            }
+                            else if (temp[j] != StringForDecoder[x])
+                            {
+                                buffer = temp[j];
+                            }
+
+                        }
+                        temp2 += buffer;
+                    }
+                    Inserted[i] = temp2;
+                }
+            return Inserted;
+        }
+        bool Result, nullbull;
+        bool chk = false;
+        public bool TestStringCorrectValue(string[] Inserted)
+        {
+            
+            Char[] StringForDecoder = new Char[] { 'у', 'е', 'ы', 'а', 'о', 'э', 'я', 'и', 'ю', 'ё', 'У', 'Е', 'Ы', 'А', 'О', 'Э', 'Я', 'И', 'Ю', 'Ё' };
+            for (int i = 0; i < Inserted.Length; i++)
+            {
+                Result = nullbull;
+                string temp = Inserted[i];
+                int lngTmp = temp.Length;
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if (chk == true)
+                    {
+                        chk = false;
+                        break;
+                    }
+                    else
+                    {
+                        for (int x = 0; x < StringForDecoder.Length; x++)
+                        {
+                            if (j + 3 > lngTmp)
+                            {
+                                chk = true;
+                                break;
+                            }
+                            else
+                            {
+                                if (temp[j] == StringForDecoder[x])
+                                {
+                                    string chck1 = temp[j].ToString() + temp[j + 1].ToString() + temp[j + 2].ToString();
+                                    string chck2 = temp[j].ToString() + "c" + temp[j].ToString();
+                                    for (int f = 0; f < chck1.Length; f++)
+                                    {
+                                        if (chck1[f] == chck2[f])
+                                        {
+                                            Result = true;
+                                        }
+                                        else
+                                        {
+                                            Result = false;
+                                        }
+                                    }
+                                    j += 3;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return Result;
+        }
+    }
+}
